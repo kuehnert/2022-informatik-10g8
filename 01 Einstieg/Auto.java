c25ee4351008ef6659b5bb0e4f00adeebe1154d8
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-18 10:25:46 +0200

    Stunde vom 18.08.2022

---
+++ 01 Einstieg/Auto.java
@@ -0,0 +1,16 @@
+public class Auto {
+    // Variablen
+    private String marke;
+    private String modell;
+    private String farbe;
+    private int baujahr; // Ganzzahl
+    private int kmStand;
+    private double preis; // Kommazahl
+    private boolean automatik; // Wahrheitswert
+    
+    // Setter-Methoden
+    public void setMarke() {
+        marke = "VW";
+        System.out.println("Setze Marke auf VW");
+    }
+}
---
+++ 01 Einstieg/Autohaendler.java
@@ -0,0 +1,8 @@
+public class Autohaendler {
+    public static void main() {
+        System.out.println("Willkommen bei Kühnerts Karren");
+        
+        Auto auto1 = new Auto();
+        auto1.setMarke();
+    }
+}
