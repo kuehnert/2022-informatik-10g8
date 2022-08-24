cf8c237709710203faec0e850a24f1bdeb940560
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-24 13:57:57 +0200

    Stunde vom 24.08.2022

--- 01 Einstieg/Auto.java
+++ 01 Einstieg/Auto.java
@@ -3,14 +3,37 @@ public class Auto {
     private String marke;
     private String modell;
     private String farbe;
-    private int baujahr; // Ganzzahl
-    private int kmStand;
-    private double preis; // Kommazahl
-    private boolean automatik; // Wahrheitswert
+    private String kennzeichen;
+    
+    // Funktionen
+    // f(x) = 2 * x
+    // f(2) = 4
+
+    public void hupe() {
+        System.out.println(kennzeichen + " Huuuuuuuuuuuup!");
+    }
     
     // Setter-Methoden
-    public void setMarke() {
-        marke = "VW";
-        System.out.println("Setze Marke auf VW");
+    public void setKennzeichen(String neuesKennzeichen) {
+        kennzeichen = neuesKennzeichen;
+    }
+
+    public void setMarke(String neueMarke) {
+        marke = neueMarke;
+    }
+    
+    public void setModell(String neuesModell) {
+        modell = neuesModell;
+    }
+    
+    public void setFarbe(String neueFarbe) {
+        farbe = neueFarbe;
+    }
+    
+    public void druckeSchild() {
+        System.out.println("Kennzeichen: " + kennzeichen);
+        System.out.println("Marke: " + marke);
+        System.out.println("Modell: " + modell);
+        System.out.println("Farbe: " + farbe);
     }
 }
--- 01 Einstieg/Autohaendler.java
+++ 01 Einstieg/Autohaendler.java
@@ -1,8 +1,30 @@
 public class Autohaendler {
-    public static void main() {
+    private Auto auto1;
+    private Auto auto2;
+    private Auto auto3;
+    
+    public void hauptprogramm() {
         System.out.println("Willkommen bei Kühnerts Karren");
         
-        Auto auto1 = new Auto();
-        auto1.setMarke();
-    }
+        auto1 = new Auto();
+        auto1.setFarbe("rot");
+        auto1.setKennzeichen("LEV MS-0");
+        auto1.setMarke("VW");
+        auto1.setModell("ID 3");
+        auto1.druckeSchild();
+        
+        auto2 = new Auto();
+        auto2.setFarbe("blau");
+        auto2.setKennzeichen("LEV MS-0");
+        auto2.setMarke("VW");
+        auto2.setModell("ID 3");
+        auto2.druckeSchild();
+
+        auto3 = new Auto();
+        auto3.setFarbe("gelb");
+        auto3.setKennzeichen("LEV MS-0");
+        auto3.setMarke("VW");
+        auto3.setModell("ID 3");
+        auto3.druckeSchild();
+}
 }
