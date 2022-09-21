6bf1d3cffa36e78040aa969c89ca726f31d738f8
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-21 13:58:05 +0200

    Stunde vom 21.09.2022

--- 01 Einstieg/Ticketautomat.java
+++ 01 Einstieg/Ticketautomat.java
@@ -1,39 +1,83 @@
+import java.util.Scanner;
+
 public class Ticketautomat {
     // Daten
+    private Scanner tastatur;
     private int ticketpreis;
-    private int guthaben;
-    private int anzahl;
-    
+    private int restbetrag;
+    private int ticketanzahl;
+
     // Funktionen
-    public void druckeTicket() {
-        
+    public Ticketautomat() {
+        tastatur = new Scanner(System.in);
+        ticketpreis = 2;
+        restbetrag = 0;
+        ticketanzahl = -1;
     }
     
-    public void druckeTickets() {
+    public void bestimmeAnzahl() {
+        System.out.print("Wie viele Tickets? ");
+        // Lies eine Zeichenkette über Tastatur ein und
+        // speichere sie in <eingabe>
+        String eingabe = tastatur.nextLine();
+        System.out.println("Deine Eingabe: '" + eingabe + "'");
         
+        // Wandle die Zeichenkette <eingabe> in eine
+        // Ganzzahl um und speichere sie in <ticketanzahl>
+        ticketanzahl = Integer.parseInt(eingabe);
+        System.out.println("Du willst: " + ticketanzahl + " Tickets");
     }
-    
-    public void einzahlen(int betrag) {
+
+    public void einzahlen() {
+        // 1. Rechne aus, wie viel der Kunde bezahlen muss
+        //    und speichere das in <restbetrag>
+        // FALSCH: ticketpreis * ticketanzahl = restbetrag;
+        // RICHTIG:
+        restbetrag = ticketpreis * ticketanzahl;
+        System.out.println("Es fehlen: " + restbetrag + " €");
         
+        // 2. Lies über Tastatur ein Geldstueck ein
+        //    und ziehe das vom <restbetrag> ab
+        System.out.print("Wirf einen Betrag ein: ");
+        String eingabe = tastatur.nextLine();
+        int geldstueck = Integer.parseInt(eingabe);
+        restbetrag = restbetrag - geldstueck;
+        
+        System.out.println("Es fehlen: " + restbetrag + " €");
+
+        // 3. ???
     }
-    
+
     public void gibWechselgeld() {
-        
+
     }
     
-    public void druckeMenu() {
+    public void druckeTicket() {
+    }
+
+    public void druckeTickets() {
+    }
+
+    public void begruesse() {
         System.out.println("❤---------------------------👍");
         System.out.println("Willkommen bei Lauras Reisen");
         System.out.println("❤---------------------------👍");
-        
-        System.out.println("Ein Ticket kostet 1€.");
+        System.out.println("Ein Ticket kostet "+ticketpreis+"€.");
     }
     
+    public void druckeMenu() {
+        begruesse();
+        bestimmeAnzahl();
+        einzahlen();
+        gibWechselgeld();
+        druckeTickets();
+    }
+
     public void druckeStatus() {
-        
+
     }
-    
+
     public void bestimmeAnzahl(int neueAnzahl) {
-        
+
     }
 }
