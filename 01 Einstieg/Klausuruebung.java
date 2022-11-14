767f5433ea7270fc8f13ea97f56423da58718b0d
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-11-14 13:15:53 +0100

    Stunde vom 14.11.2022

---
+++ 01 Einstieg/Klausuruebung.java
@@ -0,0 +1,17 @@
+public class Klausuruebung {
+    String vorname;
+    
+    public void rechnen(int x) {
+        System.out.println(2 * x + 1);
+    }
+    
+    public static void kommazahl() {
+        System.out.printf("%.2f", 3.1415);
+    }
+    
+    // Aufgabe: Gib alle Zahlen zwischen 1 und 500 aus,
+    // die durch 5 aber nicht durch 3 teilbar sind
+    
+    // Aufgabe: Gib alle Quadratzahlen aus, die zwischen
+    // 3000 und 4000 sind
+}
