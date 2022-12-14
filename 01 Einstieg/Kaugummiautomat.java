466216056dc64c1113793dd93300f35a2209978d
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-12-14 13:47:54 +0100

    Stunde vom 14.12.2022

--- 01 Einstieg/Kaugummiautomat.java
+++ 01 Einstieg/Kaugummiautomat.java
@@ -5,18 +5,21 @@ public class Kaugummiautomat {
     private int kaugummies;
     private int schoki;
     private int geld;
-    
+
     public Kaugummiautomat() {
         tastatur = new Scanner(System.in);
         kaugummies = 10;
         schoki = 5;
         geld = 0;
     }
-    
+
     public void kaufen() {
-        System.out.println("Kau (1) oder Schoki (2)? ");
+        System.out.println("Kau oder Schoki? ");
         String eingabe = tastatur.nextLine();
+        // == funktioniert nicht bei Strings
         if (eingabe == "Kau") {
+            System.out.println("Hier ist eine Million Euro!");
+        } else if (eingabe.equals("Kau")) {
             if (kaugummies > 0) {
                 System.out.println("Hier ein Kau ");
                 kaugummies -= 1;
@@ -24,8 +27,18 @@ public class Kaugummiautomat {
             } else {
                 System.out.println("Kau sind aus");
             }
-        } else {
+        } else if (eingabe.equals("Schoki")) {
             // das gleiche in grün
+            System.out.println("Hier ein Schocki.");
+        } else {
+            System.out.println("Kennschnisch.");
         }
+    }
+
+    public void status() {
+        System.out.println("Im Automaten gibt es:");
+        System.out.println(kaugummies + " x Kaugummis");
+        System.out.println(schoki + " x Schoki");
+        System.out.println(geld + " Euro");
     }
 }
--- 01 Einstieg/Zahlenraten.java
+++ 01 Einstieg/Zahlenraten.java
@@ -4,20 +4,30 @@ import java.util.Scanner;
 public class Zahlenraten {
     Random generator;
     Scanner tastatur;
-    
+
     public Zahlenraten() {
         generator  = new Random();
         tastatur = new Scanner(System.in);
     }
-    
+
+    // Hausis:
+    // 1. Anzahl der Versuche mitzaehlen und Kommentieren
+    // 2. Man soll wiederholt spielen können
     public void ratenG8() {
         int zufallszahl = generator.nextInt(100) + 1;
+        int geraten = 1;
         
-        System.out.print("Rate meine Zahl: ");
-        int geraten = tastatur.nextInt();
-        
-        if (zufallszahl < geraten) {
-            System.out.print("Meine Zahl ist kleiner!");
+        while (zufallszahl != geraten) {
+            System.out.print("Rate meine Zahl: ");
+            geraten = tastatur.nextInt();
+
+            if (zufallszahl < geraten) {
+                System.out.print("Meine Zahl ist kleiner! ");
+            } else if (zufallszahl > geraten) {
+                System.out.print("Meine Zahl ist groesser! ");
+            } else if (zufallszahl == geraten) {
+                System.out.println("Hurra, Du hast sie geraten! ");
+            }
         }
     }
 }
