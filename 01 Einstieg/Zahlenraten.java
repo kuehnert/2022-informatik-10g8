b5d82d18aaf1d3c00656c7c812726ef33286deee
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-12-15 10:10:55 +0100

    Stunde vom 15.12.2022

--- 01 Einstieg/Zahlenraten.java
+++ 01 Einstieg/Zahlenraten.java
@@ -14,20 +14,40 @@ public class Zahlenraten {
     // 1. Anzahl der Versuche mitzaehlen und Kommentieren
     // 2. Man soll wiederholt spielen können
     public void ratenG8() {
-        int zufallszahl = generator.nextInt(100) + 1;
-        int geraten = 1;
-        
-        while (zufallszahl != geraten) {
-            System.out.print("Rate meine Zahl: ");
-            geraten = tastatur.nextInt();
+        int weiter; // 1 bedeutet "weiterspielen"
 
-            if (zufallszahl < geraten) {
-                System.out.print("Meine Zahl ist kleiner! ");
-            } else if (zufallszahl > geraten) {
-                System.out.print("Meine Zahl ist groesser! ");
-            } else if (zufallszahl == geraten) {
-                System.out.println("Hurra, Du hast sie geraten! ");
+        // Aeussere Schleife zaehlt Partien
+        do {
+            int zufallszahl = generator.nextInt(100) + 1;
+            int geraten = 1;
+            int versuche = 0;
+
+            // Innere Schleife laeuft bis Zahl geraten wurde
+            while (zufallszahl != geraten) {
+                System.out.print("Rate meine Zahl: ");
+                geraten = tastatur.nextInt();
+                versuche = versuche + 1;
+
+                if (zufallszahl < geraten) {
+                    System.out.print("Meine Zahl ist kleiner! ");
+                } else if (zufallszahl > geraten) {
+                    System.out.print("Meine Zahl ist groesser! ");
+                } else if (zufallszahl == geraten) {
+                    System.out.println("Hurra, Du hast sie geraten! ");
+                }
             }
-        }
+
+            System.out.printf("Du hast %d geraten. ", versuche);
+            if (versuche <= 5) {
+                System.out.println("sehr gut (1)");
+            } else if (versuche <= 7) {
+                System.out.println("gut (2)");
+            } else {
+                System.out.println("ausreichend (4)");
+            }
+
+            System.out.print("Noch einmal spielen (1=ja)? ");
+            weiter = tastatur.nextInt();
+        } while (weiter == 1);
     }
 }
